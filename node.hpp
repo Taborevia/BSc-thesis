0010b1053100ba8fc1235edfa550347d5fee8eb6
Paweł <pawel.grzegorzewski25@gmail.com>
2024-11-15 11:36:40 +0100

    newer veresion plus some things needed to mcts

---
+++ node.hpp
@@ -0,0 +1,20 @@
+#ifndef NODE_H
+#define NODE_H
+#include <vector>
+#include <array>
+#include <memory>
+
+struct node
+{
+    std::vector<std::shared_ptr<node>> childrens;
+    std::vector<int> possible_actions; //nie zbadane wczesniej 
+    std::shared_ptr<node> parent;
+    std::array<std::array<int,11>,2> board;
+    bool player;
+    int action;
+    float reward = 0;
+    int simulations = 0;
+    node(bool maximazing_player, std::array<std::array<int,11>,2> state, std::shared_ptr<node> parent, int action_from_parent, std::vector<int> possible_actions) : player(maximazing_player), board(state), parent(parent),action(action_from_parent), possible_actions(possible_actions) {}
+};
+
+#endif
